eeb2bfb07b76d9373e47941389176a27d5f2192c
tjdreams <15221806781@163.com>
2016-07-13 16:35:47 +0800

    jquery高级教程

---
+++ 课程/高级/jquery实现/jquery-01.js
@@ -0,0 +1,10 @@
+;
+(function (window, docuemnt) {
+    var w = window,
+        doc = document;
+    var tj = function (selector) {
+        return new tj.prototype.init(selector);
+    }
+    console.log(tj);
+    window._$ = tj;
+})(window, document);
