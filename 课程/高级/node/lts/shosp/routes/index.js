0947a8acb049c726bff94a2fa24feda85d626632
tjdreams <15221806781@163.com>
2016-08-12 10:21:22 +0800

    node+angular2+less+ejs未来之光官网

--- 课程/高级/node/lts/shosp/routes/index.js
+++ 课程/高级/node/lts/shosp/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+var fs=require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var JsonObj=JSON.parse(fs.readFileSync('/index.json'));
+    console.log(JsonObj);
+    res.render('index', { title: '未来之光', project:JsonObj.a});
+
 });
+
+router.get('/login', function(req, res, next) {
+    res.render('user/login', { login: '登录' });
+});
+
+router.get('/reg', function(req, res, next) {
+    res.render('user/register', { reg: '注册' });
+});
+
+
+router.get('/reg', function(req, res, next) {
+    res.render('user/register', { reg: '注册' });
+});
+
+router.get('/reg', function(req, res, next) {
+    res.render('user/register', { reg: '注册' });
+});
+
 
 module.exports = router;
